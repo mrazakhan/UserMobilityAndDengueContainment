d30403075df3fa7d8ec93520ded88ae9ee5f2ae0
Muhammad Raza Khan <razaurrehman@gmail.com>
2016-05-02 13:02:03 -0700

    Minor

--- src/MovementStats/DailyCounts.py
+++ src/MovementStats/DailyCounts.py
@@ -22,7 +22,14 @@ def count_ab(lst, a, b):
 				count+=1
 	return count
 
-
+'''
+Counting all the entries greater than 1 as 1
+'''
+def binary_encode(x):
+	if x==0:
+		return 0
+	else 
+		return 1
 
 def dailycounter(inputfile, tower_town_file):
 		filename=inputfile
@@ -39,6 +46,11 @@ def dailycounter(inputfile, tower_town_file):
 
 		sf_union=sf_Caller.append(sf_Callee)
 
+		sf_union['Subscriber']=sf_union['Subscriber'].apply(lambda x:x.split(' ')[0]) # get rid of every thing after space
+		sf_union['Subscriber']=sf_union['Subscriber'].apply(lambda x:x.replace('00E','')) # get rid of OOE
+		sf_union['Subscriber']=sf_union['Subscriber'].apply(lambda x:x.replace('.','')) # get rid of .
+		
+
 		sf_towns=gl.SFrame.read_csv(tower_town_file, header=False)
 		sf_towns.rename({'X1':'Tower','X2':'TownName'})
 
@@ -48,32 +60,41 @@ def dailycounter(inputfile, tower_town_file):
 		sf_merged['Date']=sf_merged['DateTime'].apply(lambda x:x.split(' ')[0])
 
 		DailyVisits=sf_merged.groupby(['Date','TownName'],operations={'Subscribers':gl.aggregate.CONCAT('Subscriber')})
-
 		# Making sure that the graphlab treats the user ids as string while exporting the data
-
 		DailyVisits['Subscribers']=DailyVisits['Subscribers'].apply(lambda x:list(set(x)))
-
 		DailyVisits['Subscribers']=DailyVisits['Subscribers'].apply(lambda x:[str(each) for each in x])
-
 		DailyVisits['SubscribersCount']=DailyVisits['Subscribers'].apply(lambda x:len(x))
 
-		dates=set(DailyVisits['Date'])
+		dates=list(set(DailyVisits['Date']))+['Overall']
+		#dates=['Overall']
 		print dates
 		#sys.exit(0)
 		for dt in dates:
-			DailyVisit_x=DailyVisits.filter_by(dt,'Date')
-			#DailyVisit_x.export_csv('DailyVisits_with_subscribers.csv',quote_level=csv.QUOTE_NONE)
-			DailyVisit_x[['Date','TownName','SubscribersCount']].export_csv('DailyVisits_without_subscribers_'+str(dt)+'.csv',quote_level=csv.QUOTE_NONE)
+			print 'PROGRESS:{}'.format(dt)
+			exclude=False
+			if dt=='Overall':
+				exclude=True
+				OverallVisits=sf_merged.groupby(['TownName'],operations={'Subscribers':gl.aggregate.CONCAT('Subscriber')})
+				# Making sure that the graphlab treats the user ids as string while exporting the data
+				OverallVisits['Subscribers']=OverallVisits['Subscribers'].apply(lambda x:list(set(x)))
+				OverallVisits['Subscribers']=OverallVisits['Subscribers'].apply(lambda x:[str(each) for each in x])
+				OverallVisits['SubscribersCount']=OverallVisits['Subscribers'].apply(lambda x:len(x))
+				OverallVisits[['TownName','SubscribersCount']].export_csv('DailyVisits_without_subscribers_'+str(dt)+'.csv',quote_level=csv.QUOTE_NONE)
+				#sys.exit(0)
+			else:
+				DailyVisit_x=DailyVisits.filter_by(dt,'Date', exclude=exclude)
+				#DailyVisit_x.export_csv('DailyVisits_with_subscribers.csv',quote_level=csv.QUOTE_NONE)
+				DailyVisit_x[['Date','TownName','SubscribersCount']].export_csv('DailyVisits_without_subscribers_'+str(dt)+'.csv',quote_level=csv.QUOTE_NONE)
 		print 'Generating Transition Matrices'
 		# Generating Movement Transition Matrices
 		
 		for dt in dates:
 				summary={}
-				sf_transits=sf_merged.filter_by(dt,'Date').unstack(column=[ 'DateTime','TownName'], new_column_name='transits')
+				sf_transits=sf_merged.filter_by(dt,'Date',exclude=exclude).unstack(column=[ 'DateTime','TownName'], new_column_name='transits')#unstack creates a dictionary in the format datetime:townname
 				sf_transits2=sf_transits.groupby('Subscriber',operations={'transits':gl.aggregate.CONCAT('transits')})
-				sf_transits2['transits']=sf_transits2['transits'].apply(lambda x:update_dict(x))
-				sf_transits2['transits']=sf_transits2['transits'].apply(lambda x:sorted(x.items()))
-				sf_transits2['towns']=sf_transits2['transits'].apply(lambda a:[y for x,y  in a])
+				sf_transits2['transits']=sf_transits2['transits'].apply(lambda x:update_dict(x))# Merging all the dictionaries into one
+				sf_transits2['transits']=sf_transits2['transits'].apply(lambda x:sorted(x.items()))# Sort the entries, This works as the key for the dictionaries was the datetime
+				sf_transits2['towns']=sf_transits2['transits'].apply(lambda a:[y for x,y  in a])# Picking up the town from the sorted transits
 		#[['2015-10-01 11:23:44', 'DataGunjBakhshTown'], ['2015-10-01 17:54:47', 'RaviTown'], ['2015-10-01 17:54:52', 'RaviTown'], ['2015-10-01 17:54:58', 'RaviTown']]
 
 		# List of possible towns
@@ -86,8 +107,9 @@ def dailycounter(inputfile, tower_town_file):
 
 				for trans in transition_lst:
 					town1,town2=trans.split('-')
-					sf_transits2[trans]=sf_transits2['towns'].apply(lambda x:count_ab(x,town1,town2))
-					summary[trans]=sf_transits2[trans].sum()
+					sf_transits2[trans]=sf_transits2['towns'].apply(lambda x:count_ab(x,town1,town2)) # This is giving the visits summary, This counts all the visists from town a to town b for a user
+					sf_transits2['UserTransitions']=sf_transits2[trans].apply(lambda x:binary_encode(x)) #This is giving the user level summary, if the user made more than one visit from Town a to Town b then it will count such visits as 1
+					summary[trans]=sf_transits2[trans].sum()# Summary is a dictionary
 				with open ('SummaryTransitions_'+str(dt)+'.csv','w') as fout:
 					fout.write('Transition,Count\n')
 					for each in summary:
